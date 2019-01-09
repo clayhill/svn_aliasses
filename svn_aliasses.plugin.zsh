ce347c0bfebb426dde6bef23ea561661b1748ada
Roeland Van Lembergen <Roeland.VanLembergen@barco.com>
2019-01-09 14:16:54 +0100

    Add another svn commit alias

--- svn_aliasses.plugin.zsh
+++ svn_aliasses.plugin.zsh
@@ -4,6 +4,8 @@ alias infinity='svn up --set-depth infinity'
 alias sst='svn status'
 alias sup='svn update'
 alias sd='svn diff'
+alias sc='svn commit'
+alias scmsg='svn commit -m'
 
 function sdc() {
   svn diff $@ | colordiff
