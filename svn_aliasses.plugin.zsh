d587b3279e868444c6e7cdac437c750902da3d87
Roeland Van Lembergen <Roeland.VanLembergen@barco.com>
2019-01-14 13:54:13 +0100

    Fix svn update alias

--- svn_aliasses.plugin.zsh
+++ svn_aliasses.plugin.zsh
@@ -7,7 +7,7 @@ alias sd='svn diff'
 alias sc='svn commit'
 alias scmsg='svn commit -m'
 
-alias supdirs='files=$(ls); for f in $files;do [ -d "$f" ] || continue; cd $f; svn up --ignore-externals; cd ..; done'
+alias supdirs='for d in */; do svn up --ignore-externals $d; done'
 alias sclean='svn cleanup . --remove-unversioned && svn revert -R .'
 
 function sdc() {
