d4fae0a87dc6c02c38dd5cb5d729907bbee32601
Roeland Van Lembergen <Roeland.VanLembergen@barco.com>
2019-01-14 13:45:02 +0100

    Add alias for svn update of subdirs and svn cleanup

--- svn_aliasses.plugin.zsh
+++ svn_aliasses.plugin.zsh
@@ -7,6 +7,9 @@ alias sd='svn diff'
 alias sc='svn commit'
 alias scmsg='svn commit -m'
 
+alias supdirs='files=$(ls); for f in $files;do [ -d "$f" ] || continue; cd $f; svn up --ignore-externals; cd ..; done'
+alias sclean='svn cleanup . --remove-unversioned && svn revert -R .'
+
 function sdc() {
   svn diff $@ | colordiff
 }
